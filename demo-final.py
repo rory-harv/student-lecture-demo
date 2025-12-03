de71bbcf77f9e6ef34553024d9ba309655d675bf
rory <victoria.harvey1@icloud.com>
2025-12-03 11:30:25 -0800

    Update demo-final.py

--- demo-final.py
+++ demo-final.py
@@ -1,10 +1,14 @@
 import open3d as o3d
 import numpy as np
 from PIL import Image
-DOWNSAMPLE_FACTOR = 4
-MAX_POINTS = 50000
+
+DOWNSAMPLE_FACTOR = 4   # constant factor to downsize image
+MAX_POINTS = 50000  # constant number of max pixels/points to traverse from the image
 
 def create_color_palette_with_octree(image_path, max_depth):
+    """Takes an image path and max tree depth to create an octree and use it to develop
+    a color palette based on the averages of the pixel colors within the image."""
+
     img = Image.open(image_path).convert('RGB') # load image and convert to rgb
 
     if DOWNSAMPLE_FACTOR > 1:   # downsize the image to decrease runtime
@@ -26,8 +30,7 @@ def create_color_palette_with_octree(image_path, max_depth):
     octree.convert_from_point_cloud(pcd, size_expand=0.01)  # convert from pointcloud
 
     palette_colors = []
-    
-    visited_nodes = set()
+    visited_nodes = set()   # creates placeholder to store visited nodes in following loop
     
     for point in points:  # loops through sampled points
         leaf_node, node_info = octree.locate_leaf_node(point)   # finds a leaf node
@@ -49,8 +52,8 @@ if __name__ == '__main__':
     color_list = []
     for color in palette:
         if color not in color_list:
-            rgb_tuple = tuple(map(int, color))
-            hex_color = '#{:02x}{:02x}{:02x}'.format(*rgb_tuple)
+            rgb_tuple = tuple(map(int, color))  # gets rgb tuple of color
+            hex_color = '#{:02x}{:02x}{:02x}'.format(*rgb_tuple)    # tansforms into hex code
             print(f"RGB: {hex_color}")
             color_list.append(color)
 
